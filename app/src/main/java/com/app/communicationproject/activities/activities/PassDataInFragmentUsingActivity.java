dfa79bd97356c0d915322a01bf0fedc57617c114
Android <mohitlivetech2016@gmail.com>
2016-11-25 17:27:06 +0530

    25 Nov 16 5:26 Final Commit

--- app/src/main/java/com/app/communicationproject/activities/activities/PassDataInFragmentUsingActivity.java
+++ app/src/main/java/com/app/communicationproject/activities/activities/PassDataInFragmentUsingActivity.java
@@ -1,15 +1,20 @@
 package com.app.communicationproject.activities.activities;
 
+import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.app.communicationproject.R;
 import com.app.communicationproject.activities.fragments.FragmentFirst;
@@ -17,23 +22,22 @@ import com.app.communicationproject.activities.fragments.FragmentSecond;
 import com.app.communicationproject.activities.listner.OnFragmentInteractionListener;
 import com.app.communicationproject.activities.listner.OnSetNameListner;
 
-public class PassDataInFragmentUsingActivity extends AppCompatActivity implements OnSetNameListner {
-    Bundle bundle = new Bundle();
+public class PassDataInFragmentUsingActivity extends FragmentActivity implements FragmentFirst.OnSetNameListner1 {
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void searchName(String origin, String destination) {
+
+       FragmentSecond fragmentSecond = (FragmentSecond) getSupportFragmentManager().findFragmentById(R.id.id_Fragment2);
+
+        String result=" "+ origin + " <-  " + destination + "... " ;
+        fragmentSecond.displayName(result);
+    }
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_data_in_fragment_using);
 
     }
-
-    @Override
-    public void setName(String name) {
-
-
-
-
-    }
-
-
-
 }
